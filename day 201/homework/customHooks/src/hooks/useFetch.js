617269ebb341c5cc0d3e35dd65a10ecb161f3582
DavitiChkhikvadze <data.poshki2020@gmail.com>
2025-09-02 11:09:17 +0400

    'commit'

---
+++ day 201/homework/customHooks/src/hooks/useFetch.js
@@ -0,0 +1,38 @@
+import { useState, useEffect } from "react";
+
+export default function useFetch(url, options = {}) {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    let isMounted = true;
+
+    setLoading(true);
+    fetch(url, options)
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        if (isMounted) {
+          setData(data);
+          setError(null);
+        }
+      })
+      .catch((err) => {
+        if (isMounted) setError(err.message);
+      })
+      .finally(() => {
+        if (isMounted) setLoading(false);
+      });
+
+    return () => {
+      isMounted = false;
+    };
+  }, [url, options]);
+
+  return { data, loading, error };
+}
